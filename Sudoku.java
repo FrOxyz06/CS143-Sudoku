ea7148f383a4764c718c7cf4422c497f4c432cc7
FrOxyz06 <elmounirmohamed2006@gmail.com>
2025-04-20 15:34:48 -0700

    Add files via upload
    finished the constructor

--- Sudoku.java
+++ Sudoku.java
@@ -1,11 +1,25 @@
-public class SudokuBoard{
-    private char[][] board ; 
-    
-    public SudokuBoard(String filename){
-      
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Sudoku {
+    private char[][] board;
+
+    public Sudoku(String filename) throws FileNotFoundException {
+        board = new char[9][9];
+        Scanner scanner = new Scanner(new File(filename));
+        int row = 0;
+            while (scanner.hasNextLine() && row < 9) {
+                String line = scanner.nextLine().trim();
+                for (int col = 0; col < 9 && col < line.length(); col++) {
+                    board[row][col] = line.charAt(col);
+                }
+                row++;
+            }
+
     }
-    
-    public String toString(){
-      return "";
+
+    public String toString() {
+        return "";
     }
 }
